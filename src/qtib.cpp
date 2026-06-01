c01f638635173fc4266f8f89127fda7708fdd042
durned <nanura82@gmail.com>
2026-06-01 16:11:06 +0200

    state probability precomputations

--- src/qtib.cpp
+++ src/qtib.cpp
@@ -74,20 +74,23 @@ std::vector<oneStepBelief>& computeOneStepBeliefs(const Pomdp& model,
      * For each (unique) state amongst all states from
      * reachable osb call the bOfS method
     */
-    for (auto& osb : oneStepBeliefs) {
-        std::unordered_map<uint64_t, double> stateProbs;
+    if (precomputedProbs) {
+        for (auto& osb : oneStepBeliefs) {
+            probDist stateProbs;
 
-        for (auto& reachable : osb.reachableBeliefs) {
-            auto s = reachable.s;
+            for (auto it = osb.reachableBeliefs.begin(); it != osb.reachableBeliefs.end(); ++it) {
+                auto s = oneStepBeliefs[*it].s;
 
-            auto it = stateProbs.find(s);
-            if (it == stateProbs.end()) {
-                // key does not exist
-                stateProbs.emplace(s, osb.bOfS(s, stateObservations));
+                auto probIter = stateProbs.find(s);
+                if (probIter == stateProbs.end()) {
+                    // key does not exist yet,
+                    // compute and put
+                    stateProbs.emplace(s, osb.bOfS(s, stateObservations));
+                }
             }
-        }
 
-        osb.stateProbs = stateProbs;
+            osb.stateProbs = stateProbs;
+        }
     }
 
     return oneStepBeliefs;
